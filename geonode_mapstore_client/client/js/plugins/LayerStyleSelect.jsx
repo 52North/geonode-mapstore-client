f397162a254faaea2092b2ece0fd3bc729e24792
JohannesSchnell <j.schnell@52north.org>
2022-11-24 16:17:18 +0100

    start LSS

--- geonode_mapstore_client/client/js/plugins/LayerStyleSelect.jsx
+++ geonode_mapstore_client/client/js/plugins/LayerStyleSelect.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { createPlugin } from "@mapstore/framework/utils/PluginsUtils";
 
+console.log("hallo maggus");
+
 function LayerStyleSelect(props) {
   console.log("test");
+  return <div></div>;
 }
 
 export default createPlugin("LayerStyleSelect", {
-  component: LayerStyleSelectPlugin,
+  component: LayerStyleSelect,
 });
