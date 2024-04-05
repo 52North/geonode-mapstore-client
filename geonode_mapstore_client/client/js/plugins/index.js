3a468285b9e418fbeadf595a942d96a7fc42c82e
Henning Bredel <h.bredel@52north.org>
2024-04-05 17:18:01 +0200

    Initiate LitterAssessment extension

--- geonode_mapstore_client/client/js/plugins/index.js
+++ geonode_mapstore_client/client/js/plugins/index.js
@@ -194,6 +194,10 @@ export const plugins = {
         'Share',
         () => import(/* webpackChunkName: 'plugins/share-plugin' */ '@js/plugins/Share')
     ),
+    LitterAssessmentPlugin: toModulePlugin(
+        'LitterAssessment',
+        () => import(/* webpackChunkName: 'plugins/litterassessment-plugin' */ '@js/plugins/LitterAssessment')
+    ),
     IdentifyPlugin: toModulePlugin(
         'Identify',
         () => import(/* webpackChunkName: 'plugins/identify-plugin' */ '@mapstore/framework/plugins/Identify')
